9a71199106e13e7e375c66be7041c6987166e3aa
haxybaxy <zaidsaheb4@gmail.com>
2025-03-21 18:15:02 +0100

    removed content search decoration utils

--- src/utils/decorationUtils.ts
+++ src/utils/decorationUtils.ts
@@ -1,9 +1,7 @@
 import * as vscode from 'vscode';
-import { escapeRegExp } from './fileUtils';
 
 export class DecorationManager {
     private lastHighlightDecoration?: vscode.TextEditorDecorationType;
-    private contentMatchDecorations?: vscode.TextEditorDecorationType;
 
     /**
      * Clear all applied decorations
@@ -12,11 +10,6 @@ export class DecorationManager {
         if (this.lastHighlightDecoration) {
             this.lastHighlightDecoration.dispose();
             this.lastHighlightDecoration = undefined;
-        }
-        
-        if (this.contentMatchDecorations) {
-            this.contentMatchDecorations.dispose();
-            this.contentMatchDecorations = undefined;
         }
     }
 
@@ -30,32 +23,5 @@ export class DecorationManager {
             isWholeLine: true
         });
         editor.setDecorations(this.lastHighlightDecoration, [lineRange]);
-    }
-
-    /**
-     * Highlight all matches of a search term in the editor
-     */
-    public highlightSearchMatches(editor: vscode.TextEditor, searchText: string): void {
-        // Find all matches in the file
-        const fileText = editor.document.getText();
-        const ranges: vscode.Range[] = [];
-        const searchRegex = new RegExp(escapeRegExp(searchText), 'gi');
-        
-        let match;
-        while ((match = searchRegex.exec(fileText)) !== null) {
-            const startPos = editor.document.positionAt(match.index);
-            const endPos = editor.document.positionAt(match.index + match[0].length);
-            ranges.push(new vscode.Range(startPos, endPos));
-        }
-        
-        // Add decorations
-        this.contentMatchDecorations = vscode.window.createTextEditorDecorationType({
-            backgroundColor: new vscode.ThemeColor('editor.findMatchHighlightBackground'),
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            borderColor: new vscode.ThemeColor('editor.findMatchHighlightBorder')
-        });
-        
-        editor.setDecorations(this.contentMatchDecorations, ranges);
     }
 } 
