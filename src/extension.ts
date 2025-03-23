f9298c1e3106edd3fd01770780acfd0a2fa914a2
haxybaxy <zaidsaheb4@gmail.com>
2025-03-23 23:29:51 +0100

    removed old code

--- src/extension.ts
+++ src/extension.ts
@@ -4,35 +4,6 @@ import * as vscode from 'vscode';
 import { EditorHistoryManager } from './lib/editorHistory';
 import { QuickOpenProvider } from './lib/quickOpenProvider';
 
-// Type definition for our custom quick pick items
-interface SearchQuickPickItem extends vscode.QuickPickItem {
-	data?: {
-		filePath: string;
-		linePos: number;
-		colPos: number;
-		searchText?: string;
-		type: 'file' | 'content';
-		lineText?: string;
-	};
-}
-
-// Track editor history for MRU functionality
-interface EditorHistoryItem {
-	uri: vscode.Uri;
-	timestamp: number;
-	linePos?: number;
-	colPos?: number;
-}
-
-// Binary file extensions to skip when attempting text operations
-const binaryFileExtensions = new Set([
-	'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.ico', '.webp', '.tiff', '.svg',
-	'.pdf', '.exe', '.dll', '.so', '.dylib', '.bin', '.dat',
-	'.zip', '.tar', '.gz', '.bz2', '.xz', '.rar', '.7z',
-	'.mp3', '.mp4', '.avi', '.mov', '.mkv', '.flv', '.webm',
-	'.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx',
-	'.class', '.pyc', '.o', '.a'
-]);
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
